db617805fcf395032eb126d62531c025861484d7
Jordi Fernandez <jordi@ywarp.com>
2017-08-19 20:14:31 +0100

    Remove AWS keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -27,15 +27,15 @@ if Rails.env.in?(%w(test development))
 else
 
   CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-        provider: "AWS",
-        region: 'eu-west-2',
-        aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
-        }
-    config.fog_directory = ENV['FOG_DIRECTORY']
-    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.days.to_i}" }
+    # config.fog_provider = 'fog/aws'
+    # config.fog_credentials = {
+    #     provider: "AWS",
+    #     region: 'eu-west-2',
+    #     aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    #     aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
+    #     }
+    # config.fog_directory = ENV['FOG_DIRECTORY']
+    # config.fog_attributes = { 'Cache-Control' => "max-age=#{365.days.to_i}" }
   end
 
 end
