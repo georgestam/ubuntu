cb4d34c212ce3efd27215c638266cb43b10e8e8b
Jordi Fernandez <jordi@ywarp.com>
2017-08-19 18:24:06 +0100

    Basic Implementation of sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  # sidekiq setup
+  require "sidekiq/web"
+  authenticate :user, lambda { |u| u.admin } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
     
