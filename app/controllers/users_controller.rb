6cd8a300629a9ecf13be0b6a8dd28852e8ddf382
Jordi Fernandez <jordi@ywarp.com>
2017-08-20 17:00:58 +0100

    Basic implementation of slack

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,17 @@ class UsersController < ApplicationController
   end
   
   def update_db
+    
     authorize(current_user)
     # pull users database and create new users if they are not in the database
     CreateUsersParser.update_customer_db
     # create alerts if users have negative accounts 
     Alert.check_customers_with_negative_acount
+    
+    client = Slack::Web::Client.new
+    client.auth_test
+    client.chat_postMessage(channel: '#general', text: 'Hello World', as_user: true)
+    
     flash[:notice] = "database updated successfully!"
     redirect_to users_path
   end 
