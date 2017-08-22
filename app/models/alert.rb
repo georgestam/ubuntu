b51b897b71bc0350b28cb2be3269defff518a051
Jordi Fernandez <jordi@ywarp.com>
2017-08-22 09:27:03 +0100

    Alerts#new view completed

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -6,7 +6,7 @@ class Alert < ApplicationRecord
   validates :customer, presence: true
   
   after_create :send_alert_email
-  after_create :send_slack_notification
+  after_create :send_slack_notification, unless: :development?
 
   def send_alert_email
     AlertMailer.perform(self).deliver_later
