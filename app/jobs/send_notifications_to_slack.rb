600bf1a3456113e06cd6cb9e2dad243326770ff6
Jordi Fernandez <jordi@ywarp.com>
2017-08-25 12:35:19 +0200

    Change logs from rubocop

--- app/jobs/send_notifications_to_slack.rb
+++ app/jobs/send_notifications_to_slack.rb
@@ -2,8 +2,8 @@ class SendNotificationsToSlack < ApplicationJob
   queue_as :default
 
   def perform(alert_id)
-    puts "I'm starting jobs"
+    logger.info { "I'm starting jobs" } 
     Alert.slack_API_call(alert_id)
-    puts "OK I'm done now"
+    logger.info { "OK I'm done now" }
   end
 end
--- app/jobs/update_db_job.rb
+++ app/jobs/update_db_job.rb
@@ -2,9 +2,9 @@ class UpdateDbJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    puts "I'm starting jobs"
+    logger.info { "I'm starting jobs" } 
     CreateUsersParser.update_customer_db
     Alert.check_customers_with_negative_acount
-    puts "OK I'm done now"
+    logger.info { "OK I'm done now" }
   end
 end
