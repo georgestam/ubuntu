283b93ea867330c563ff3b4596ce849c88e8c638
Jordi Fernandez <jordi@ywarp.com>
2017-08-27 16:10:01 +0200

    Create alert factory

---
+++ spec/factories/alert_factory.rb
@@ -0,0 +1,22 @@
+FactoryGirl.define do
+  
+  factory :alert do
+    
+    customer
+    type_alert
+    status
+    customer_id { SecureRandom.hex(8) }
+    description { Faker::ChuckNorris.fact }
+    created_by { Faker::Name.first_name }
+    assigned_to { Faker::Name.first_name }
+    resolved_comments { Faker::Lorem.sentence(3) }
+    # closed_at { rand(10.years).ago }
+    # resolved_at { rand(10.years).ago }
+      
+  end
+  
+end
+
+
+   
+  
