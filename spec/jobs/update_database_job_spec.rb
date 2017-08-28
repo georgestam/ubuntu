edbfe7993a9a28de44b1518436eeb8279b08cd8a
Jordi Fernandez <jordi@ywarp.com>
2017-08-28 19:25:21 +0200

    Basic implementation of updatejob

---
+++ spec/jobs/update_database_job_spec.rb
@@ -0,0 +1,15 @@
+describe UpdateDbJob do
+  
+  # to do!
+    
+  it "calls the update_customer_db method once the job is called" do
+    # expect(CreateUsersParser).to receive(:update_customer_db).at_least(1).times.and_call_original
+    # UpdateDbJob.perform_later 
+  end
+  
+  it "calls the check_customers_with_negative_acount method once the job is called" do
+    # expect(Alert).to receive(:check_customers_with_negative_acount).at_least(1).times.and_call_original
+    # UpdateDbJob.perform_later 
+  end
+  
+end 
