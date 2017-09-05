a372ae34a0d0b065a0a3f3cfda793e68b5377664
Jordi Fernandez <jordi@ywarp.com>
2017-09-05 11:37:30 +0200

    Add traits for roles to user#factory

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -2,12 +2,25 @@ FactoryGirl.define do
   
   factory :user do
     
+    name { Faker::Name.first_name }
     email { Faker::Internet.email }
     password { SecureRandom.hex }
     
     trait :admin do 
       admin true
-    end 
+    end
+    
+    trait :manager do 
+      role User::ROLES[0] #manager
+    end
+    
+    trait :field do 
+      role User::ROLES[1] #super
+    end
+    
+    trait :super do 
+      role User::ROLES[2] #field
+    end     
       
   end
   
