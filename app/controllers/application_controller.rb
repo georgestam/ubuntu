f533cb8f676c16098e1464fd6305ac9778c9d055
Jordi Fernandez <jordi@ywarp.com>
2017-09-16 18:21:29 +0100

    Add current_user to be avaliable in models as Current.user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   skip_before_action :verify_authenticity_token
+  around_action :set_current_user
 
   include Pundit
 
@@ -16,5 +17,15 @@ class ApplicationController < ActionController::Base
   def default_url_options
     { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
   end
+
+  # Required to use current_user in model
+  # https://stackoverflow.com/questions/2513383/access-current-user-in-model
+  def set_current_user
+    Current.user = current_user
+    yield
+  ensure
+    # to address the thread variable leak issues in Puma/Thin webserver
+    Current.user = nil
+  end      
 
 end
