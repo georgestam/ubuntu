8b61c9b4b9e68e770fd515eed2e0539c1c7660b9
Jordi Fernandez <jordi@ywarp.com>
2017-09-16 17:16:37 +0100

    Implement spec for pages#info

---
+++ spec/features/pages/info_spec.rb
@@ -0,0 +1,15 @@
+describe "Show pages#info" do
+
+context 'when user is signed-in as a manager' do 
+  
+    sign_as :manager
+    
+    it 'displays main info' do
+      visit root_path
+      find("#info").click
+      expect(page).to have_selector('#notes')
+    end
+    
+  end
+
+end 
