6e7e839eea6320dd45effe4aa5b9ba27ffb781a3
Jordi Fernandez <jordi@ywarp.com>
2017-09-18 23:28:30 +0100

    Create users in spec

--- spec/jobs/send_slack_notification_spec.rb
+++ spec/jobs/send_slack_notification_spec.rb
@@ -1,5 +1,6 @@
 describe SendNotificationsToSlack do
 
+  let!(:user){ FactoryGirl.create :user }
   let(:slack_notifier)  { double(Slack::Web::Client) }
   let!(:alert){ FactoryGirl.create :alert }
 
@@ -8,7 +9,7 @@ describe SendNotificationsToSlack do
     # 
     text = "New alert created by #{alert.created_by.name if alert.created_by.present?} for Customer #{alert.customer.first_name} #{alert.customer.last_name}: #{alert.type_alert.name}"
     expect(slack_notifier).to receive(:chat_postMessage).with(channel: '#alerts', text: text, as_user: 'ubuntu')
-    # SendNotificationsToSlack.perform_now(alert.id)
+    SendNotificationsToSlack.perform_now(alert.id)
   end
 
 end 
