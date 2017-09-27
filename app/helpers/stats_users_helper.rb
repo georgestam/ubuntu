4da9b1d60252535088212f93e7f94506d341689e
Jordi Fernandez <jordi@ywarp.com>
2017-09-27 14:07:47 +0100

    Reload graph when a tab is clicked

--- app/helpers/stats_users_helper.rb
+++ app/helpers/stats_users_helper.rb
@@ -2,7 +2,7 @@ module StatsUsersHelper
   
   def alerts_by_user
     data = Alert.all.joins(:user).group("users.name").count
-    column_chart data, xtitle: "", ytitle: ""
+    column_chart data,  id: "alerts-by-user", xtitle: "", ytitle: ""
   end
   
 end
