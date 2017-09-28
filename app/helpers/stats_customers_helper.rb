e66deb9920952809ae220ae5a6283cd3decd9ebe
Jordi Fernandez <jordi@ywarp.com>
2017-09-28 13:00:32 +0100

    Add top-10-customers-with-more-alerts graphs

--- app/helpers/stats_customers_helper.rb
+++ app/helpers/stats_customers_helper.rb
@@ -1,3 +1,14 @@
 module StatsCustomersHelper
-  
+  def top_10_customers_with_more_alerts
+    data = Alert.joins(:customer).group("customers.id").count.map{|costumer_id, count| [costumer_id ? Customer.find(costumer_id).name : "Unassigned", count] } # https://github.com/ankane/chartkick/issues/19
+    bar_chart data, id: "top-10-customers-with-more-alerts",  xtitle: "", ytitle: "", library: {
+      scales: {
+          xAxes: [{
+              ticks: {
+                  fixedStepSize: 1 # remove decimals
+              }
+          }],
+      },
+    }
+  end 
 end
