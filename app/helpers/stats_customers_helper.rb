fdde013346a3a53e82fcf827d9c9a17d5ac37ff2
Jordi Fernandez <jordi@ywarp.com>
2017-09-28 14:28:53 +0100

    Refactoring codeclimate offenses

--- app/helpers/stats_customers_helper.rb
+++ app/helpers/stats_customers_helper.rb
@@ -1,8 +1,12 @@
 module StatsCustomersHelper
-  def top_10_customers_with_more_alerts
+  def top_10_customers(order)
     data = Alert.joins(:customer).group("customers.id").count.map{|costumer_id, count| [costumer_id ? Customer.find(costumer_id).name : "Unassigned", count] } # https://github.com/ankane/chartkick/issues/19
-    top_10_data = data.sort {|a, b| b[1] <=> a[1]}.first 10 # https://stackoverflow.com/questions/9615850/ruby-sort-array-of-an-array
-    bar_chart top_10_data, id: "top-10-customers-with-more-alerts",  xtitle: "", ytitle: "", library: {
+    top_10_data = if order == "asc"
+      data.sort {|a, b| b[1] <=> a[1]}.first 10 # https://stackoverflow.com/questions/9615850/ruby-sort-array-of-an-array
+      else
+      data.sort {|a, b| a[1] <=> b[1]}.first 10
+    end 
+    bar_chart top_10_data, id: order == "asc" ? "top-10-customers-with-more-alerts" : "top-10-customers-with-less-alerts",  xtitle: "", ytitle: "", library: {
       scales: {
           xAxes: [{
               ticks: {
@@ -13,19 +17,6 @@ module StatsCustomersHelper
     }
   end
   
-  def top_10_customers_with_less_alerts
-    data = Alert.joins(:customer).group("customers.id").count.map{|costumer_id, count| [costumer_id ? Customer.find(costumer_id).name : "Unassigned", count] } # https://github.com/ankane/chartkick/issues/19
-    top_10_data = data.sort {|a, b| a[1] <=> b[1]}.first 10 # https://stackoverflow.com/questions/9615850/ruby-sort-array-of-an-array
-    bar_chart top_10_data, id: "top-10-customers-with-less-alerts",  xtitle: "", ytitle: "", library: {
-      scales: {
-          xAxes: [{
-              ticks: {
-                  fixedStepSize: 1 # remove decimals
-              }
-          }]
-      }
-    }
-  end
 end
 
 
