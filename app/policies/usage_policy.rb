8ca12147f70db406655c67a96dcbef25ad23e15c
Jordi Fernandez <jordi@ywarp.com>
2017-09-29 21:07:01 +0100

    Add autorizations to meters and usages in rails admin

--- app/policies/usage_policy.rb
+++ app/policies/usage_policy.rb
@@ -1,3 +1,19 @@
 class UsagePolicy < ApplicationPolicy
+  
+  def new? 
+    false
+  end 
+  
+  def edit? 
+    false
+  end 
+  
+  def show? 
+    false
+  end 
+  
+  def destroy?
+    false
+  end 
     
 end
