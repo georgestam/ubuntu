91892a132fdf8b23260ad709ab916b13ea951904
Jordi Fernandez <jordi@ywarp.com>
2017-09-29 21:54:51 +0100

    Made URL for pull usage dynamic

--- app/models/usage.rb
+++ app/models/usage.rb
@@ -7,8 +7,8 @@ class Usage < ApplicationRecord
   validates :created_on, uniqueness: {scope: :meter}, allow_nil: true
   
   def self.request_usage_to_api(date, meter_id)
-    url = "https://api.steama.co/customers/1474778/utilities/1/usage/?end_time=2017-09-26&format=json&start_time=2017-09-025T00%3A00%3A00"
-  
+    customer = Meter.find(meter_id).customer
+    url = "https://api.steama.co/customers/#{customer.id_steama}/utilities/1/usage/?end_time=2017-09-26&format=json&start_time=2017-09-025T00%3A00%3A00"
     json_data = if !test?  
       body = RestClient.get( url,
       {
