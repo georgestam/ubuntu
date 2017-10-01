2894643a570c88c2379ab7652ef17ef01b7555f5
Jordi Fernandez <jordi@ywarp.com>
2017-10-01 21:04:29 +0100

    Implement any? instead of present?

--- app/jobs/send_daily_reminders_to_slack_job.rb
+++ app/jobs/send_daily_reminders_to_slack_job.rb
@@ -7,7 +7,7 @@ class SendDailyRemindersToSlackJob < ApplicationJob
       Alert.notify_open_alerts_to_slack(User.find_by(slack_username: "@jordi"))
     else
       User.all.each do |user|
-        Alert.notify_open_alerts_to_slack(user) if Alert.all_open.where(user_id: user.id).present?
+        Alert.notify_open_alerts_to_slack(user) if Alert.all_open.where(user_id: user.id).any?
       end
     end  
     logger.info { "OK I'm done now" }
