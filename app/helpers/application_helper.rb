dde4f7847f11c7b97e4a99fa046a56598868bee3
Jordi Fernandez <jordi@ywarp.com>
2017-10-02 19:34:49 +0100

    Implement in alerts stats range of dates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,15 +12,16 @@ module ApplicationHelper
     {
       title: {
            display: true,
-           fontSize: 15,
+           fontSize: 12,
+           padding: 50,
            text: "#{title} - #{@start_date.strftime('%d %b %Y') } to #{@end_date.strftime('%d %b %Y') }"
        }
     }
   end
   
-  def by_day(records, group_by)
+  def by_week(records, group_by)
     opts = [group_by, {range: @start_date..@end_date, format: '%d %b'}]
-    method_name = :group_by_day
+    method_name = :group_by_week
     if by_year?
       opts[1].merge!({format: '%Y'})
       method_name = :group_by_year
@@ -39,5 +40,9 @@ module ApplicationHelper
   def by_month?
     @end_date - (3.month + 2.days) > @start_date
   end
+  
+  def select_range_of_dates_for(records)
+    records.where(:created_at => @start_date.beginning_of_day..@end_date.end_of_day)
+  end 
 
 end 
--- app/helpers/stats_alerts_helper.rb
+++ app/helpers/stats_alerts_helper.rb
@@ -5,12 +5,12 @@ module StatsAlertsHelper
     # @alerts = @customer.alerts
   
     alerts = Alert.all
-    line_chart by_day(alerts), basic_opts('Total alerts')
+    line_chart by_week(alerts), basic_opts('Total alerts')
   end
   
   def total_alerts_in_time
-    alerts_created = by_day(Alert.all, "created_at")
-    alerts_resolved = by_day(Alert.all, "resolved_at")
+    alerts_created = by_week(Alert.all, "created_at")
+    alerts_resolved = by_week(Alert.all, "resolved_at")
     
     data = [
       {name: "Created Alerts", data: alerts_created},
@@ -18,35 +18,34 @@ module StatsAlertsHelper
     ]
     column_chart data, id: "total-alerts-in-time", legend: "bottom", xtitle: "weeks", ytitle: "", library: basic_opts('Total alerts')
   end
+  
+  def alerts_by_time
+    data = GroupAlert.all.map { |group_alert| 
+      {name: group_alert.title, data: by_week(group_alert.alerts, "alerts.created_at")} 
+    }
+    column_chart data, legend: "bottom", xtitle: "weeks", ytitle: "", library: basic_opts('Number of Alerts (Alerts Group)')
+  end 
+  
+  def alerts_by_group_alert_donut
+    data = select_range_of_dates_for(GroupAlert.all).joins(:alerts).group("group_alerts.title").count
+    pie_chart data, donut: true, legend: "bottom", library: basic_opts('% By alert group')
+  end
 
   def alerts_by_type_alert
-    data = Alert.all.joins(:type_alert).group("type_alerts.id").count.map{|type_alert_id, count| [type_alert_id ? truncate_string(TypeAlert.find(type_alert_id).name) : "Unassigned", count] } # https://github.com/ankane/chartkick/issues/19
-    bar_chart data, xtitle: "", ytitle: ""
+    data = select_range_of_dates_for(Alert.all).joins(:type_alert).group("type_alerts.id").count.map{|type_alert_id, count| [type_alert_id ? truncate_string(TypeAlert.find(type_alert_id).name) : "Unassigned", count] } # https://github.com/ankane/chartkick/issues/19
+    bar_chart data, xtitle: "", ytitle: "", library: basic_opts('Alerts description (total)')
   end
   
   def alerts_by_group_alert
-    data = GroupAlert.joins(:alerts).group("group_alerts.title").count
-    bar_chart data
+    data = select_range_of_dates_for(GroupAlert.all).joins(:alerts).group("group_alerts.title").count
+    bar_chart data, library: basic_opts('Alert group (total)')
   end
-  
-  def alerts_by_group_alert_donut
-    data = GroupAlert.joins(:alerts).group("group_alerts.title").count
-    pie_chart data, donut: true, legend: "bottom"
-  end
-  
-  def alerts_by_time
-  data = GroupAlert.all.map { |group_alert| 
-    {name: group_alert.title, data: group_alert.alerts.group_by_week("alerts.created_at").count} 
-  }
-  column_chart data, legend: "bottom", xtitle: "weeks", ytitle: ""
-  end 
   
   def top_10_solutions
-    
-    data = Alert.all_resolved.joins(:issue).group("issues.id").count.map{|issue_id, count| [issue_id ? truncate_string(Issue.find(issue_id).name) : "Unassigned", count] } # https://github.com/ankane/chartkick/issues/19
+    data = select_range_of_dates_for(Alert.all_resolved).joins(:issue).group("issues.id").count.map{|issue_id, count| [issue_id ? truncate_string(Issue.find(issue_id).name) : "Unassigned", count] } # https://github.com/ankane/chartkick/issues/19
     top_10_data = data.sort {|a, b| b[1] <=> a[1]}.first 10 # https://stackoverflow.com/questions/9615850/ruby-sort-array-of-an-array
     
-    bar_chart top_10_data, id: "Top_10_solutions",  xtitle: "", ytitle: ""
+    bar_chart top_10_data, id: "Top_10_solutions", library: basic_opts('Top 10 Solutions')
   end
   
 end
