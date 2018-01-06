325050a0a806b8549f57ee07ba7390adbf77d6a5
Jordi Fernandez <jordi@ywarp.com>
2018-01-06 12:46:08 +0100

    correct rubocop offenses

--- db/migrate/20180105222242_add_unique_index_to_balances.rb
+++ db/migrate/20180105222242_add_unique_index_to_balances.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndexToBalances < ActiveRecord::Migration[5.0]
   def change
-    add_index :balances, [:created_on, :customer_id], unique: true
+    add_index :balances, %i[created_on customer_id], unique: true
   end
 end
