e08a06e50a082206822b311a70b315738f87a1b3
Jordi Fernandez <jordi@respira.io>
2018-07-18 22:57:19 +0100

    Comment negative account alerts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     # pull users database and create new users if they are not in the database
     Customer.update_customer_db
     # create alerts if users have negative accounts 
-    Alert.check_customers_with_negative_acount
+    # Alert.check_customers_with_negative_acount
     
     flash[:notice] = "database updated successfully!"
     redirect_to root_path
--- app/jobs/update_db_job.rb
+++ app/jobs/update_db_job.rb
@@ -5,7 +5,7 @@ class UpdateDbJob < ApplicationJob
     logger.info { "I'm starting jobs" } 
     Customer.update_customer_db
     Balance.update_balance
-    Alert.check_customers_with_negative_acount
+    # Alert.check_customers_with_negative_acount
     logger.info { "OK I'm done now" }
   end
 end
