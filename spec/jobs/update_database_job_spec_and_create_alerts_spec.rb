7df39a9bafa575b2da80715922efbb8ad13edba6
Jordi Fernandez <jordi@respira.io>
2018-07-19 09:10:55 +0100

    Remove test for negative account

--- spec/jobs/update_database_job_spec_and_create_alerts_spec.rb
+++ spec/jobs/update_database_job_spec_and_create_alerts_spec.rb
@@ -9,10 +9,10 @@ describe UpdateDbJob do
     UpdateDbJob.perform_now 
   end
   
-  it "calls the check_customers_with_negative_acount method once the job is called" do
-    expect(Alert).to receive(:check_customers_with_negative_acount).at_least(1).times.and_call_original
-    UpdateDbJob.perform_now 
-  end
+  # it "calls the check_customers_with_negative_acount method once the job is called" do
+  #   expect(Alert).to receive(:check_customers_with_negative_acount).at_least(1).times.and_call_original
+  #   UpdateDbJob.perform_now 
+  # end
   
   it "calls the update_balance method once the job is called" do
     expect(Balance).to receive(:update_balance).at_least(1).times.and_call_original
